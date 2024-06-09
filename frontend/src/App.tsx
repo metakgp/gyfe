b1ec526cf5a9373636d1c15858ae8b9c2f0e8fe5
Dami-18 <damiyagondha@gmail.com>
2024-06-09 22:33:54 +0530

    add pnpm as package manager

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,6 +1,6 @@
 // import { useState } from 'react'
 import React from 'react';
-import './styles/App.css'
+import './styles/App.scss'
 import Header from './components/Header';
 import Footer from './components/Footer';
 import Form from './components/Form'
