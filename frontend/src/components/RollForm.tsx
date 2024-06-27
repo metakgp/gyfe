0228a56c5199e730cff22162e8752b72d21f5843
Dami-18 <damiyagondha@gmail.com>
2024-06-28 01:46:22 +0530

    complete login process using api

--- frontend/src/components/RollForm.tsx
+++ frontend/src/components/RollForm.tsx
@@ -2,15 +2,16 @@ import React from 'react';
 import { useForm } from 'react-hook-form';
 import * as yup from "yup";
 import { yupResolver } from "@hookform/resolvers/yup";
+import { BACKEND_URL } from './url';
 
 interface IFormInput {
-  roll: string;
+  roll_number: string;
   passwd: string;
 }
 
 //validation
 const schema = yup.object().shape({
-  roll: yup.string().required("Roll number is required!").matches(/^\d{2}[A-Z]{2}\d{5}$/, "Please enter valid roll number!"),
+  roll_number: yup.string().required("Roll number is required!").matches(/^\d{2}[A-Z]{2}\d{5}$/, "Please enter valid roll number!"),
   passwd: yup.string().required("Password is required!")
 });
 
@@ -23,19 +24,38 @@ const RollForm: React.FC<FormProps> = ({onSubmit}) => {
 
     const {register, handleSubmit, formState: {errors}} = useForm<IFormInput> ({ resolver: yupResolver(schema)});
 
-    const handleFormSubmit = (data: IFormInput) => {
+    const handleFormSubmit = async (data: IFormInput) => {
       console.log(data); // Or send data to an API endpoint
-      sessionStorage.setItem("roll",data.roll);
-      sessionStorage.setItem("passwd",data.passwd);
+      sessionStorage.setItem("passwd",data.passwd); // Hash the password, then store. Stored for use in next form
+      try {
+        const response = await fetch(`${BACKEND_URL}/secret-question`, {
+          method: "POST",
+          headers: { "Content-Type": "application/json" },
+          body: JSON.stringify(data),
+        })
+    
+        if (!response.ok) {
+          throw new Error(`Error: ${response.statusText}`);
+        }
+    
+        const responseData = await response.json(); // returns jwt and secret question
+        sessionStorage.setItem("jwt",responseData.jwt) //store jwt
+        sessionStorage.setItem("secret_question",responseData.secret_question) // use to display in next form
+        onSubmit();
+      } 
+      catch (error) {
+        console.error("Error fetching secret question:", error); // Here handle error. Can show that ErrorPage.tsx
+      }
+      // sessionStorage.setItem("roll_number",data.roll_number);
       onSubmit();
     }
 
     return (
-        <form onSubmit={handleSubmit(handleFormSubmit)} method='POST'>
+        <form onSubmit={handleSubmit(handleFormSubmit)}>
             <div className='roll'>
               <label>Roll number: </label>
-              <input type="text" placeholder='Roll number for ERP, e.g. 22AE10024' className='input-box' {...register("roll")}/>
-              {errors.roll && <p style={{color: "red"}}>{errors.roll.message}</p>}
+              <input type="text" placeholder='Roll number for ERP, e.g. 22AE10024' className='input-box' {...register("roll_number")}/>
+              {errors.roll_number && <p style={{color: "red"}}>{errors.roll_number.message}</p>}
             </div>
             <div className='passwd'>
               <label>Password: </label>
--- frontend/src/components/SecurityQueForm.tsx
+++ frontend/src/components/SecurityQueForm.tsx
@@ -3,6 +3,7 @@ import { useForm } from 'react-hook-form';
 import * as yup from 'yup';
 import { yupResolver } from '@hookform/resolvers/yup';
 import { useNavigate } from 'react-router-dom';
+import { BACKEND_URL } from './url';
 
 const schema = yup.object().shape({
   securityAnswer: yup.string().required('Security answer is required'),
@@ -22,27 +23,70 @@ const SecurityQueForm: React.FC<SecurityQueFormProps> = ({updateStatus}) => {
   
     const {register, handleSubmit, getValues, formState: {errors},} = useForm<IFormInput>({resolver: yupResolver(schema)});
 
-    const getOTP = (event: React.MouseEvent<HTMLButtonElement, MouseEvent>) => {
+    const getOTP = async (event: React.MouseEvent<HTMLButtonElement, MouseEvent>) => {
+      console.log("this called")
       event.preventDefault(); // To Prevent reload
-      // const securityAnswer = getValues('securityAnswer'); //Send this to an API and fetch otp
-      console.log("Get otp"); 
+      const security_answer = getValues('securityAnswer'); 
+      const passwd = sessionStorage.getItem("passwd"); // hadnle the hashed password
+
+      const formData = {
+        secret_answer: security_answer,
+        password: passwd,
+      }
+
+      try {
+        const response = await fetch(`${BACKEND_URL}/request-otp`, {
+          method: "POST",
+          headers: { "Content-Type": "application/json" },
+          body: JSON.stringify(formData),
+        });
+    
+        if (!response.ok) {
+          throw new Error(`Error: ${response.statusText}`);
+        }
+
+        const responseData = await response.json();
+        console.log(responseData.message); // Or can show this message using react-hot-toast
+    
+      } catch (error) {
+        console.error("Error fetching OTP:", error); // To show error page
+      }
     }
     
     const navigate = useNavigate();
     
-    const onSubmit = () => {
-      const otp = getValues('otp'); //Send this otp to api
-      console.log(otp);
-      console.log(sessionStorage.getItem("roll"));
-      console.log(sessionStorage.getItem("passwd"));
-      updateStatus(); //call this method only if response is OK from api
-      navigate('/');
+    const onSubmit = async () => {
+      const otp1 = getValues('otp'); 
+      const otp_data = {
+        otp: otp1,
+      }
+      try {
+        const response = await fetch(`${BACKEND_URL}/login`, {
+          method: "POST",
+          headers: { "Content-Type": "application/json" },
+          body: JSON.stringify(otp_data),  // In api, this endpoint requests password and secret answer again. But I have given only otp. Can be changed later
+        });
+    
+        if (!response.ok) {
+          throw new Error(`Error: ${response.statusText}`);
+        }
+
+        const responseData = await response.json();
+        console.log(responseData.message); // Or can show this message using react-hot-toast
+
+        updateStatus(); 
+        navigate('/');
+    
+      } 
+      catch (error) {
+        console.error("Error fetching OTP:", error); // To show error page
+      }
     }
 
     return (
         <form className='login-form' onSubmit={handleSubmit(onSubmit)}>
             <div className='question'>
-              <label>Security question: </label>  
+              <label>{sessionStorage.getItem("secret_question")}: </label>  
               <input type="text" placeholder='Enter your answer' className='input-box box' {...register('securityAnswer')}></input>
               {errors.securityAnswer && (<p style={{ color: 'red' }}>{errors.securityAnswer.message}</p>)}
             </div>
