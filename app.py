9108df097ef8e60ffe05d2d73ab9162ab1ef92c1
premagarwals <prem12937@gmail.com>
2024-06-30 13:53:52 +0530

    update: made requested changes

--- app.py
+++ app.py
@@ -1,13 +1,22 @@
-from flask import Flask, render_template, request, send_file, jsonify
-import requests
+from flask import Flask, request, send_file, jsonify
 import logging
 from iitkgp_erp_login import session_manager
 import gyfe
 import io
+import os
+import jwt as jwt_gen
+from dotenv import load_dotenv
+from datetime import datetime
+
 app = Flask(__name__)
 
+load_dotenv()
+app.config['JWT_SECRET_KEY'] = os.getenv('JWT_SECRET_KEY')
 
-jwt_secret_key :str = "secret-key"
+if not app.config['JWT_SECRET_KEY']:
+    raise ValueError("No JWT_SECRET_KEY set for Flask application")
+
+jwt_secret_key = app.config['JWT_SECRET_KEY']
 headers = {
         "timeout": "20",
         "User-Agent": "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Ubuntu Chromium/51.0.2704.79 Chrome/51.0.2704.79 Safari/537.36",
@@ -127,8 +136,8 @@ def logout():
     except Exception as e:
         return ErpResponse(False, str(e), status_code=500).to_response()
 
-@app.route('/fetch-csvfile', methods=["POST"])
-def fetch_csvfile():
+@app.route('/elective/<ELECTIVE>', methods=["GET"])
+def elective(ELECTIVE):
     try:
         jwt = None
         auth_resp, status_code = handle_auth()
@@ -137,49 +146,51 @@ def fetch_csvfile():
         else:
             jwt = auth_resp.get_json().get("jwt")
         
-        data = request.get_json()
-        SESSION = data.get('session')
-        SEMESTER = data.get('semester')
-        YEAR = int(data.get('year'))
-        ELECTIVES = data.get('electives')
-        DEPT = data.get('dept')
+        ROLL_NUMBER = jwt_gen.decode(jwt, jwt_secret_key, algorithms=['HS256'])["roll_number"]
+
+        YEAR = int(ROLL_NUMBER[1])
+        DEPT = ROLL_NUMBER[2:4]
+        current_month = datetime.now().month
+        if current_month in [12, 1, 2, 3, 4, 5]:
+            SEMESTER = "SPRING"
+            SESSION = str(datetime.now().year - 1) +'-'+ str(datetime.now().year)
+        elif current_month in [6, 7, 8, 9, 10, 11]:
+            SEMESTER = "AUTUMN"
+            SESSION = str(datetime.now().year) +'-'+ str(datetime.now().year + 1)
         
-        if not all([SESSION, SEMESTER, YEAR, ELECTIVES]):
-            return ErpResponse(False, "Missing args to create the csv file.", status_code=400).to_response()
-        
-        headers = {
-            "timeout": "20",
-            "User-Agent": "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Ubuntu Chromium/51.0.2704.79 Chrome/51.0.2704.79 Safari/537.36",
-        }
+        """
+        save_depth and save_breadth needs 2 responses, that is PRIMARY_RESP and SUBJ_LIST_RESP,
+        both function calls find_core_courses inside them, which requires COURSES_RESP to run.
+
+        fetching all the responses and making a tuple named "response" and passing to gyfe.save_depths() or gyfe.save_breadths().
+        """
 
         TIMETABLE_URL = f"https://erp.iitkgp.ac.in/Acad/view/dept_final_timetable.jsp?action=second&course={DEPT}&session={SESSION}&index={YEAR}&semester={SEMESTER}&dept={DEPT}"
         ERP_ELECTIVES_URL = "https://erp.iitkgp.ac.in/Acad/central_breadth_tt.jsp"
 
-        if ELECTIVES== "depth":
+        if ELECTIVE== "depth":
             SUBJ_LIST_URL = f"https://erp.iitkgp.ac.in/Acad/timetable_track.jsp?action=second&for_session={SESSION}&for_semester={SEMESTER}&dept={DEPT}"
-            response_1 = session_manager.request(jwt=jwt, method='GET', url=TIMETABLE_URL, headers=headers)
-        
-        elif ELECTIVES == "breadth":
+            PRIMARY_RESP = session_manager.request(jwt=jwt, method='GET', url=TIMETABLE_URL, headers=headers)
+        elif ELECTIVE == "breadth":
             SUBJ_LIST_URL = f"https://erp.iitkgp.ac.in/Acad/timetable_track.jsp?action=second&dept={DEPT}"
-            response_1 = session_manager.request(jwt=jwt, method='GET', url=ERP_ELECTIVES_URL, headers=headers)
-
+            PRIMARY_RESP = session_manager.request(jwt=jwt, method='GET', url=ERP_ELECTIVES_URL, headers=headers)
         else:
-            return ErpResponse(False, "Invalid elective type.", status_code=400).to_response()
+            return ErpResponse(False, "Invalid elective type.", status_code=404).to_response()
         
         semester = 2 * YEAR - 1 if SEMESTER=="AUTUMN" else 2 * YEAR
         COURSES_URL = f"https://erp.iitkgp.ac.in/Academic/student_performance_details_ug.htm?semno={semester}"
 
-        response_2 = session_manager.request(jwt=jwt, method='GET', url=SUBJ_LIST_URL, headers=headers)
-        response_3 = session_manager.request(jwt=jwt, method='POST', url=COURSES_URL, headers=headers)
+        SUBJ_LIST_RESP = session_manager.request(jwt=jwt, method='GET', url=SUBJ_LIST_URL, headers=headers)
+        COURSES_RESP = session_manager.request(jwt=jwt, method='POST', url=COURSES_URL, headers=headers)
 
-        response = (response_1, response_2, response_3)
+        response = (PRIMARY_RESP, SUBJ_LIST_RESP, COURSES_RESP) 
 
         if not all(response):
             return ErpResponse(False, "Failed to retrieve data..", status_code=500).to_response()
 
-        if ELECTIVES == "breadth":
+        if ELECTIVE == "breadth":
             file = gyfe.save_breadths(response, False)
-        elif ELECTIVES == "depth":
+        elif ELECTIVE == "depth":
             file = gyfe.save_depths(response, False)
         
         csv = io.BytesIO()
@@ -190,13 +201,8 @@ def fetch_csvfile():
             csv,
             as_attachment=True,
             mimetype='text/csv',
-            download_name=f"$my_{ELECTIVES}.csv"
+            download_name=f"${ELECTIVE}_{ROLL_NUMBER}.csv"
         )
 
-
     except Exception as e:
-        logging.error(f" {e}")
         return ErpResponse(False, str(e), status_code=500).to_response()
-    
-    
-    
