1965f57c67a284b2082bbcb3257e0018fc190a9f
Prem Agarwal <85096702+premagarwals@users.noreply.github.com>
2024-07-04 07:35:35 +0000

    update: changes in jwt_secret_key handling
    Co-authored-by: Rohan Barsagade <82886259+rohan-b-84@users.noreply.github.com>

--- app.py
+++ app.py
@@ -11,12 +11,10 @@ from datetime import datetime
 app = Flask(__name__)
 
 load_dotenv()
-app.config['JWT_SECRET_KEY'] = os.getenv('JWT_SECRET_KEY')
+jwt_secret_key = os.getenv('JWT_SECRET_KEY')
 
-if not app.config['JWT_SECRET_KEY']:
+if not jwt_secret_key:
     raise ValueError("No JWT_SECRET_KEY set for Flask application")
-
-jwt_secret_key = app.config['JWT_SECRET_KEY']
 headers = {
         "timeout": "20",
         "User-Agent": "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Ubuntu Chromium/51.0.2704.79 Chrome/51.0.2704.79 Safari/537.36",
