3f035d4d0ec8063c3eac4e783356e2773f11a1a3
Rohan Barsagade <rbarsagade101@gmail.com>
2024-07-11 03:23:53 +0530

    feat: add CORS (allow all origin)

--- app.py
+++ app.py
@@ -6,9 +6,9 @@ import gyfe
 import io
 from datetime import datetime
 import requests
-
+from flask_cors import CORS
 app = Flask(__name__)
-
+CORS(app)
 headers = {
         "timeout": "20",
         "User-Agent": "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Ubuntu Chromium/51.0.2704.79 Chrome/51.0.2704.79 Safari/537.36",
